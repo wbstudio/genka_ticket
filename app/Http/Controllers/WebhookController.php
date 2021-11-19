492ee8559cc33ad91d0e40713062076d31eed23c
matsu-motsu <s.matsumoto5270@gmail.com>
2021-11-19 06:10:38 +0000

    Stripe用webhookの実装

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,22 +5,47 @@ namespace App\Http\Controllers;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\DB;
+use \App\Models\Subscription;
+use Carbon\Carbon;
 
 class WebhookController extends CashierController
 {
     public function handleCustomerSubscriptionDeleted($payload)
     {
-        // $event = \Stripe\Event::constructFrom(
-        //     json_decode($payload, true)
-        // );
+        try {
+            // サブスクリプションID取得
+            $subscriptionId = $payload['data']['subscription'];
 
-        Log::debug($payload);
+            // サブスクリプションエンティティ取得
+            $mbSubscription = new Subscription();
+            $subscription = $mbSubscription
+                ->where([
+                    'stripe_subscription_key' => $subscriptionId,
+                    'end_on'                  => null,
+                    'delete_flag'             => 0
+                ])
+                ->first();
 
-        // サブスクリプションID取得
-        // サブスクリプションエンティティ取得
-        // キャンセル処理
+            // キャンセル処理
+            if (is_null($subscription)) {
+                throw new \Exception("no target subscription");
+            }
+            $todayObj = Carbon::now();
+            $subscription->end_on = $todayObj->format('Y-m-d');
+            $subscription->delete_flag = 1;
 
+            $subscription->saveOrFail();
+        } catch (\Exception $e) {
+            Log::debug($e->getMessage());
+            echo($e->getMessage());
+
+            // 失敗レスポンス
+            http_response_code(500);
+            exit;
+        }
+
+        // 成功レスポンス
         http_response_code(200);
     }
 }
